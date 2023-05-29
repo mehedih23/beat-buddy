812ad26b6f23766eabe69a283a8065cc90aa8e1a
mehedih23 <nmehedi23@gmail.com>
2023-05-29 08:10:05 +0600

    basic modified & header file added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../assets/beat-buddy-logo.png'
+
+const Header = () => {
+    return (
+        <div className='flex items-center justify-center flex-col md:flex-row md:items-center md:justify-between bg-green-200'>
+            <div>
+                <a href="/home">
+                    <img src={logo} alt="Beat Buddy" />
+                </a>
+            </div>
+            <div className='flex flex-col md:flex-row'>
+                <a className='my-2 md:my-0 mx-0 md:mx-4' href="/home">Home</a>
+                <a className='my-2 md:my-0 mx-0 md:mx-4' href="/shop">Shop</a>
+                <a className='my-2 md:my-0 mx-0 md:mx-4' href="/orders">Orders</a>
+                <a className='my-2 md:my-0 mx-0 md:mx-4' href="/about">About</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
