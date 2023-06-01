91ab6670c4ed7d64fb7e2ebcd1dc800cc00c11c6
mehedih23 <nmehedi23@gmail.com>
2023-06-01 22:09:19 +0600

    modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,11 @@ const Header = () => {
     return (
         <div className='flex items-center justify-center flex-col md:flex-row md:items-center md:justify-between bg-green-200'>
             <div>
-                <a href="/home">
+                <a href="/beat">
                     <img src={logo} alt="Beat Buddy" />
                 </a>
             </div>
             <div className='flex flex-col md:flex-row'>
-                <a className='my-2 md:my-0 mx-0 md:mx-4' href="/home">Home</a>
                 <a className='my-2 md:my-0 mx-0 md:mx-4' href="/shop">Shop</a>
                 <a className='my-2 md:my-0 mx-0 md:mx-4' href="/orders">Orders</a>
                 <a className='my-2 md:my-0 mx-0 md:mx-4' href="/about">About</a>
